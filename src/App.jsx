3de31ee3d5845b4038c517761cee22afb7ebcecd
Abhishek Silawat <absilawat@gmail.com>
2022-09-23 16:16:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import "./App.css";
 import { Gyms } from "./components/Gyms";
 
 import { Route, Routes } from "react-router-dom";
-import { Gympage } from "./components/gympage/Gympage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Gyms />}></Route>
-        <Route exact path="/gymdetails" element={<Gympage />}></Route>
       </Routes>
     </div>
   );
